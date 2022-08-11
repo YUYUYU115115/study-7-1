041957bbe100776a4d04803c3aa0d4df1c1abd8e
YUYUYU115115 <Yuisun1235@gmail.com>
2022-08-11 18:17:01 +0900

    [add]記事編集画面

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +10,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
+Route::get('/posts/{post}', 'PostController@show');
+Route::post('/posts', 'PostController@store');
